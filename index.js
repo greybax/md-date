083b71ef7da0c81bd6f9b5efe52e679f6ca264f6
greybax <greybax@gmail.com>
2016-07-20 18:47:20 -0700

    v1.0.2 using remark-helpers

--- index.js
+++ index.js
@@ -1,59 +1,32 @@
-import remark   from "remark";
-import html     from "remark-html";
-import moment   from "moment";
-import removeMd from "remove-markdown";
+import moment from "moment";
+import { ast, text, html, isParagraph } from "remark-helpers";
 
-// TODO: Need to move it to helper functions
 const isDate = (format, locale, node) => {
-    let paragraph = "";
-    for (let i = 0; i < node.children.length; i++) {
-        paragraph += getParagraphPlainText(node.children[i], paragraph);
-    }
-    return moment(paragraph, format, locale, true).isValid();
-}
-
-// TODO: Need to move it to helper functions
-const isParagraph = (node) => {
-    return (node.type === "paragraph");
-}
-
-// TODO: Need to move it to helper functions
-const getParagraphPlainText = (node, paragraph) => {
-    if (node.value) {
-        paragraph = node.value;
-    }
-    if (node.children) {
-        return getParagraphPlainText(node.children[0], paragraph);
-    }
-
-    return paragraph;
+    return moment(text(node), format, locale, true).isValid();
 }
 
 export default (format, locale, input) => {
     if (!input)
         return;
 
-    let ast = remark().parse(input).children;
-    let clonedAst = { type: "root", children: [] };
+    let mdast = ast(input).children;
+    let clonedMdast = { type: "root", children: [] };
 
-    for (let i = 0; i < ast.length; i++) {
-        if (isParagraph(ast[i]) && isDate(format, locale, ast[i])) {
-            clonedAst.children.push(ast[i]);
+    for (let i = 0; i < mdast.length; i++) {
+        if (isParagraph(mdast[i]) && isDate(format, locale, mdast[i])) {
+            clonedMdast.children.push(mdast[i]);
         }
     };
 
-    if (clonedAst.children.length === 0)
+    if (clonedMdast.children.length === 0)
         return;
 
-    let md = remark().stringify(clonedAst).trim();
-    let dom = remark().use(html).process(md).contents.trim();
-    let plainText = removeMd(md).trim();
-
+    let plainText = text(clonedMdast);
     return {
         text: plainText,
-        html: dom,
+        html: html(clonedMdast),
         unix: moment.utc(plainText, format, locale, true).unix(),
         moment: moment(plainText, format, locale, true),
-        clonedAst
+        clonedMdast
     };
 };
